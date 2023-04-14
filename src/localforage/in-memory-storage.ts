00a54b539b0bb75958b7076a61d04a249c8b3dc8
nick thomas <nichoth@gmail.com>
2023-04-14 16:39:58 -0700

    test runs

--- src/localforage/in-memory-storage.ts
+++ src/localforage/in-memory-storage.ts
@@ -5,42 +5,42 @@
 export class Storage {
   storage: { [key: string]: any}
 
-  constructor() {
-    this.storage = {}
+  constructor () {
+      this.storage = {}
   }
 
   getItem = <T>(key: string): Promise<T | null> => {
-    return new Promise(resolve => {
-      const val = this.storage[key] || null
+      return new Promise(resolve => {
+          const val = this.storage[key] || null
 
-      // https://localforage.github.io/localForage/#data-api-getitem
-      // "Even if undefined is saved, null will be returned by getItem().
-      // This is due to a limitation in localStorage, and for compatibility
-      // reasons localForage cannot store the value undefined."
-      const checkedVal = val === undefined ? null : val
+          // https://localforage.github.io/localForage/#data-api-getitem
+          // "Even if undefined is saved, null will be returned by getItem().
+          // This is due to a limitation in localStorage, and for compatibility
+          // reasons localForage cannot store the value undefined."
+          const checkedVal = val === undefined ? null : val
 
-      resolve(checkedVal)
-    })
+          resolve(checkedVal)
+      })
   }
 
   setItem = <T>(key: string, val: T): Promise<T> => {
-    return new Promise(resolve => {
-      this.storage[key] = val
-      resolve(val)
-    })
+      return new Promise(resolve => {
+          this.storage[key] = val
+          resolve(val)
+      })
   }
 
   removeItem = (key: string): Promise<void> => {
-    return new Promise(resolve => {
-      delete this.storage[key]
-      resolve()
-    })
+      return new Promise(resolve => {
+          delete this.storage[key]
+          resolve()
+      })
   }
 
   clear = (): Promise<void> => {
-    return new Promise(resolve => {
-      this.storage = {}
-      resolve()
-    })
+      return new Promise(resolve => {
+          this.storage = {}
+          resolve()
+      })
   }
 }
